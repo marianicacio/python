c661b9a1bb571fe10c0e292ed15dab6c0412cb5b
marianicacio <mariaeduarda.nicacio2007@gmail.com>
2024-07-16 17:18:53 -0300

    feat: aula-8-2

--- aula-8.py
+++ aula-8.py
@@ -56,62 +56,66 @@ resp = int(input("Escolha: \n1 - Adição\n2 - Subtração\n3 - Multiplicação\
 n1 = int(input("N1: "))
 n2 = int(input("N2: "))
 
-
-if resp == 1:
-    res = soma(n1,n2)
-    print(f"Soma: {res}")
+while True:
+    if resp == 1:
+        res = soma(n1,n2)
+        print(f"Soma: {res}")
    
 
-elif resp == 2:
-    res = dif(n1,n2)
-    print(f"Subtração: {res}")
+    elif resp == 2:
+        res = dif(n1,n2)
+        print(f"Subtração: {res}")
    
 
-elif resp == 3:
-    print(f"Multiplicação: " , mult(n1,n2))
+    elif resp == 3:
+        print(f"Multiplicação: " , mult(n1,n2))
    
 
-elif resp == 4:
-    print(f"Divisão:  + {div(n1,n2)}")
+    elif resp == 4:
+        print(f"Divisão:  + {div(n1,n2)}")
    
-elif resp == 5:
-    print(f"Exponenciação:  + {div(n1,n2)}")
-   
-   
-elif resp == 6:
-    print(f"Resto de Divisão:  + {div(n1,n2)}")
+    elif resp == 5:
+        print(f"Exponenciação:  + {div(n1,n2)}")
    
    
-elif resp == 7:
-    if parImpar(n1) == 1:
+    elif resp == 6:
+        print(f"Resto de Divisão:  + {div(n1,n2)}")
+   
+   
+    elif resp == 7:
+        if parImpar(n1) == 1:
             print(f"{n1} é par")
-    elif parImpar(n1) == 0:
+        elif parImpar(n1) == 0:
             print(f"{n1} é impar")
-    if parImpar(n2) == 1:
+        if parImpar(n2) == 1:
             print(f"{n2} é par")
-    elif parImpar(n2) == 0:
+        elif parImpar(n2) == 0:
             print(f"{n2} é impar")
        
        
-elif resp == 8:
-    if primo(n1) == 1 and primo(n2) == 1:
-        print(f"{n1} e {n2} são primos")  
-    elif primo(n1) == 1 and primo(n2) == 0:
-         print(f"{n1} é primo {n2} não é primo")      
-    elif primo(n1) == 0 and primo(n2) == 1:
-         print(f"{n1} não é primo {n2} é primo")      
-    elif primo(n1) == 0 and primo(n2) == 0:
-         print(f"{n1} e {n2} não são primos") 
+    elif resp == 8:
+        if primo(n1) == 1 and primo(n2) == 1:
+            print(f"{n1} e {n2} são primos")  
+        elif primo(n1) == 1 and primo(n2) == 0:
+            print(f"{n1} é primo {n2} não é primo")      
+        elif primo(n1) == 0 and primo(n2) == 1:
+            print(f"{n1} não é primo {n2} é primo")      
+        elif primo(n1) == 0 and primo(n2) == 0:
+            print(f"{n1} e {n2} não são primos") 
        
-elif resp == 9:
-    print(f"Fatorial de {n1}: {fatorial(n1)}")
-    print(f"Fatorial de {n2}: {fatorial(n2)}")
-       
-       
-elif resp == 10:
-    tabuada(n1)
-    tabuada(n2)
+    elif resp == 9:
+        print(f"Fatorial de {n1}: {fatorial(n1)}")
+        print(f"Fatorial de {n2}: {fatorial(n2)}")
        
        
-else: 
-    print("opção inválida")
+    elif resp == 10:
+        tabuada(n1)
+        print()
+        tabuada(n2)
+
+    opc = int(input("Se quiser sair digite 0"))
+    if(opc!=0):
+        print("Escolheu continuar")
+    else:
+        print("Sair")
+    break
